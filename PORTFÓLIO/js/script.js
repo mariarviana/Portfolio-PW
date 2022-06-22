f56cc22c538407b8ce7c6add4939140dfb84ada4
Maria Eduarda Ribeiro Viana <viana.ma.ribeiro@gmail.com>
2022-06-21 20:22:51 -0400

    update
    first submit

---
+++ PORTFÓLIO/js/script.js
@@ -0,0 +1,15 @@
+function imgCriancaTati(imagem){
+    imagem.src="img/tati-feltrin-crianca.jpg";
+}
+
+function imgAdultoTati(imagem){
+    imagem.src="img/tati-feltrin.jpg";
+}
+
+function imgIsa(imagem){
+    imagem.src="img/isa-lubrano-2.jpg";
+}
+
+function imgIsaDois(imagem){
+    imagem.src="img/isabella-lubrano.jpg";
+}
